01ee17accfdc0e0ac86830316b6c4e13cef4371e
Scott Robinson <45150413+Mooky4591@users.noreply.github.com>
2020-07-29 16:08:44 -0400

    Add files via upload

---
+++ GameOverPanel.java
@@ -0,0 +1,32 @@
+package SnakeGame;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GameOverPanel extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static int WIDTH = 100, HEIGHT = 100;
+	
+public GameOverPanel() {
+
+		setFocusable(true);
+		
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+	}
+
+//private void setFocusable(boolean b) {
+	// TODO Auto-generated method stub
+	
+//}
+
+//private void setPreferredSize(Dimension dimension) {
+	// TODO Auto-generated method stub
+	
+//}
+
+}
---
+++ Main.java
@@ -0,0 +1,29 @@
+package SnakeGame;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public Main() {
+	
+	JFrame frame = new JFrame();
+	GamePanel gamepanel = new GamePanel();
+	
+	frame.add(gamepanel);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setTitle("SNAKE");
+	frame.setLocationRelativeTo(null);
+	
+	frame.pack();
+	frame.setVisible(true);
+	
+	}
+
+	public static void main(String[] args) {
+		
+		new Main();
+		
+	}
+
+
+}
